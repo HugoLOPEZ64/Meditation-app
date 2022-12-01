f76b0fc9ab5e7e95028162aa18714dde7ff14fff
Hugo LOPEZ <hugo.lopez002@gmail.com>
2022-12-01 23:35:56 +0100

    add function button play and pause

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const app = () => {
 
         outline.style.strokeDasharray = outlineLength;
         outline.style.strokeDashoffset = outlineLength;
+
+        play.addEventListener("click", () => {
+            checkPlaying(song);
+        });
+
+        const checkPlaying = song =>{
+
+        if(song.paused){
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        }else{
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+        }
+    
 };
 
 app();
