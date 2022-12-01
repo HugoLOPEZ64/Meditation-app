f07498d4dacb232c400b5449afe86fc3703b4e22
Hugo LOPEZ <hugo.lopez002@gmail.com>
2022-12-02 00:47:51 +0100

    project finished, need to make it responsive

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = () => {
 
     const sounds = document.querySelectorAll('.sound-picker button');
 
-    const timeDisplay = document.querySelectorAll('.time-display');
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
 
     const outlineLength = outline.getTotalLength();
 
@@ -15,8 +16,23 @@ const app = () => {
         outline.style.strokeDasharray = outlineLength;
         outline.style.strokeDashoffset = outlineLength;
 
+        sounds.forEach(sound =>{
+            sound.addEventListener('click', function (){
+                song.src = this.getAttribute('data-sound');
+                video.src = this.getAttribute('data-video');
+                checkPlaying(song);
+            });
+        });
+
         play.addEventListener("click", () => {
             checkPlaying(song);
+        });
+
+        timeSelect.forEach(option =>{
+            option.addEventListener('click', function(){
+                fakeDuration = this.getAttribute('data-time');
+                timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+            });
         });
 
         const checkPlaying = song =>{
@@ -29,8 +45,27 @@ const app = () => {
             song.pause();
             video.pause();
             play.src = './svg/play.svg';
+            }
+        };
+
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if(currentTime >= fakeDuration){
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
         }
-        }
+    };
     
 };
 
